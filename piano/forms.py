26b7a2a78a29455ec4f721fb429d5ab2ce4ac035
Adrien Lemaire <lemaire.adrien@gmail.com>
2010-08-14 18:46:16 +0100

    piano views and forms created

---
+++ piano/forms.py
@@ -0,0 +1,30 @@
+#from django
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+ 
+#from trackstore
+from trackstore.models import Track
+ 
+class TrackForm(forms.ModelForm):
+    """
+    Track Form: form associated to the Track model
+    """
+ 
+    def __init__(self, *args, **kwargs):
+        super(TrackForm, self).__init__(*args, **kwargs)
+        self.is_update = False
+ 
+    def clean(self):
+        """ Do validation stuff. """
+        # title is mandatory
+        if 'title' not in self.cleaned_data:
+            return
+        # if a track with that title already exists...
+        if not self.is_update:
+            if Track.objects.filter(title=self.cleaned_data['title']).count() > 0:
+                raise forms.ValidationError(_("There is already this track in the library."))
+        return self.cleaned_data
+ 
+    class Meta:
+        model = Track
+        fields = ('composer', 'publisher', 'author', 'description', 'title', 'tags')
