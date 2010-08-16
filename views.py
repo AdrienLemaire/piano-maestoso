e3de2e197508626408e788194ffe3c9297d92a8e
Adrien Lemaire <lemaire.adrien@gmail.com>
2010-08-17 00:51:55 +0100

    error fixed in home view

--- views.py
+++ views.py
@@ -11,6 +11,9 @@ RE_BROWSER = re.compile(r'Firefox/(\d)')
 def home(request):
     """ Send a png image if the browser doesn't support css background
     property with svg """
-    extra_context = {'svg_background' : not int(RE_BROWSER.search(request.META['HTTP_USER_AGENT']).group(1)) < 4}
-    return render_to_response("homepage.html", extra_context,
+    svg_background = True
+    user_agent = RE_BROWSER.search(request.META['HTTP_USER_AGENT'])
+    if user_agent:
+        svg_background = not int(user_agent.group(1)) < 4
+    return render_to_response("homepage.html", {"svg_background": svg_background},
                               context_instance=RequestContext(request))
