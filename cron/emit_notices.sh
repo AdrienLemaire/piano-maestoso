af3fd529536b363e315f879e1ef77b1f89137487
Adrien Lemaire <lemaire.adrien@gmail.com>
2010-08-19 23:42:43 +0100

    cron scripts created to send mail and notices

---
+++ cron/emit_notices.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script works only in linux
+# You should put your virtualenv in ~/Envs
+WORKON_HOME=$(readlink -f ~/Envs)
+PROJECT_ROOT=$(readlink -f ..)
+
+# activate virtual environment
+. $WORKON_HOME/pinax-env/bin/activate
+
+python manage.py emit_notices >> $PROJECT_ROOT/logs/cron_mail.log 2>&1
