997ebbc2f92a1a7e5e680905ad4f859b1c4f92c4
Adrien Lemaire <lemaire.adrien@gmail.com>
2010-08-20 15:49:33 +0100

    css improvements

--- media/js/tabs.js
+++ media/js/tabs.js
@@ -1,13 +1,13 @@
 // Too slow
 //$(function() {
 //});
-$("#right_tabs li").each(function(index) {
-    if (index % 2 != 0) {
-        $(this).css({
-            backgroundColor: "#FFF"
-        });
-        $(this).children("a").css({
-            color: "#000",
-        });
-    }
-});
+//$("#right_tabs li").each(function(index) {
+    //if (index % 2 != 0) {
+        //$(this).css({
+            //backgroundColor: "#FFF"
+        //});
+        //$(this).children("a").css({
+            //color: "#000",
+        //});
+    //}
+//});
