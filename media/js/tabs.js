ece675eaa696df3ec27db1e95134292326ac9478
Adrien Lemaire <lemaire.adrien@gmail.com>
2010-08-27 11:29:07 +0200

    messages div moved to tab_head

--- media/js/tabs.js
+++ media/js/tabs.js
@@ -11,7 +11,6 @@ var nb_pianokey = 11;
 var pianokey_height = $(".white").outerHeight();
 var pianokey_white_width = $(".white").outerWidth();
 var pianokey_black_adjust = pianokey_white_width * 0.11; // Why 0.11 ? mystery
-//var pianokey_black_width = pianokey_white_width * 0.6;
 
 
 // We define some useful variables
@@ -21,19 +20,12 @@ var rope_length = Math.sqrt(Math.pow(border_radius, 2) * 2);
 var arc_length = 2 * border_radius * Math.asin(rope_length / (2 * border_radius));
 var piano_length = max_height + arc_length + max_width - (nb_pianokey + 1) * (pianokey_height + pianokey_margin);
 var piano_keys_to_add = piano_length / (pianokey_height + pianokey_margin)
-//var pianokey_sounds = Array("b1", "c2", "c1", "cs1", "d1", "ds1", "e1", "f1",
-    //"fs1", "g1", "gs1", "a1", "as1");
 var pianokey_sounds = Array("b1", "as1", "a1", "gs1", "g1", "fs1", "f1", "e1", "ds1",
         "d1", "cs1", "c1");
 
 
-
 // We add new pianokeys to fill the piano keyboard
 for (i=0; i<piano_keys_to_add; i++) {
-    //liClass = "black";
-    //if (i % ((pianokey_height+pianokey_margin) * 2) == 0) {
-        //liClass = "white";
-    //}
     note_class = pianokey_sounds[i % pianokey_sounds.length];
     liClass = (note_class.length == 2) ? "white " + note_class: "black " + note_class;
     $("#right_tabs ul").append("<li class='" + liClass + "'>&nbsp;</li>");
@@ -45,9 +37,7 @@ $("#right_tabs ul>li").each(function(i){
     // We define the top value of the pianokey
     pianokey_top = i * (pianokey_height + pianokey_margin);
     diff_max_height = -(max_height - (pianokey_top + pianokey_height));
-    if (diff_max_height < 0) {
-        diff_max_height = 0;
-    }
+    if (diff_max_height < 0) diff_max_height = 0;
 
     // We define the value for moving the pianokey to the left
     pianokey_left = diff_max_height;
@@ -59,18 +49,18 @@ $("#right_tabs ul>li").each(function(i){
         deg = 90 - rad * (180 / Math.PI);
         if (deg > 0 && deg < 90) {
             // We ajust the position due to the rotation
-            // TODO NOT WORKING !!!
             arc_distance = border_radius - Math.sqrt(Math.pow((border_radius - diff_max_height), 2) +
                 Math.pow((pianokey_top - max_height), 2));
-            pianokey_left -= arc_distance;
-            pianokey_top += arc_distance;
+            //pianokey_left -= arc_distance;
+            //pianokey_top += arc_distance;
+            // bad guess to arrange a bit the rotation
+            pianokey_left -= arc_distance / 2;
+            pianokey_top += arc_distance / 2;
+            deg = 0.9 * deg; 
         }
         else if (deg > 90) {
             // deg and pianokey_top are static values
             lag = (this.classList[0] == "white") ? 0 : pianokey_black_adjust;
-            //if (pianokey_top % ((pianokey_height + pianokey_margin) * 2) == 0) {
-                //lag = (pianokey_white_width - pianokey_black_width) / 4;
-            //}
             pianokey_top = max_height + border_radius + pianokey_height + pianokey_margin - lag;
             pianokey_left -= lag;
             deg = 90;
