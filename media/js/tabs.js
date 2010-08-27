29e73abe8cc66f6dedc97cd786d86fbf0e5266e0
Adrien Lemaire <lemaire.adrien@gmail.com>
2010-08-27 11:06:15 +0200

    sounds in pianokeys onmouseover

--- media/js/tabs.js
+++ media/js/tabs.js
@@ -10,7 +10,8 @@ var nb_pianokey = 11;
 // pianokey width and height
 var pianokey_height = $(".white").outerHeight();
 var pianokey_white_width = $(".white").outerWidth();
-var pianokey_black_width = pianokey_white_width * 0.6;
+var pianokey_black_adjust = pianokey_white_width * 0.11; // Why 0.11 ? mystery
+//var pianokey_black_width = pianokey_white_width * 0.6;
 
 
 // We define some useful variables
@@ -19,14 +20,22 @@ var max_height = $("#body_block").outerHeight() - border_radius;
 var rope_length = Math.sqrt(Math.pow(border_radius, 2) * 2);
 var arc_length = 2 * border_radius * Math.asin(rope_length / (2 * border_radius));
 var piano_length = max_height + arc_length + max_width - (nb_pianokey + 1) * (pianokey_height + pianokey_margin);
+var piano_keys_to_add = piano_length / (pianokey_height + pianokey_margin)
+//var pianokey_sounds = Array("b1", "c2", "c1", "cs1", "d1", "ds1", "e1", "f1",
+    //"fs1", "g1", "gs1", "a1", "as1");
+var pianokey_sounds = Array("b1", "as1", "a1", "gs1", "g1", "fs1", "f1", "e1", "ds1",
+        "d1", "cs1", "c1");
+
 
 
 // We add new pianokeys to fill the piano keyboard
-for (i=0; i<piano_length; i=i+pianokey_height+pianokey_margin) {
-    liClass = "black";
-    if (i % ((pianokey_height+pianokey_margin) * 2) == 0) {
-        liClass = "white";
-    }
+for (i=0; i<piano_keys_to_add; i++) {
+    //liClass = "black";
+    //if (i % ((pianokey_height+pianokey_margin) * 2) == 0) {
+        //liClass = "white";
+    //}
+    note_class = pianokey_sounds[i % pianokey_sounds.length];
+    liClass = (note_class.length == 2) ? "white " + note_class: "black " + note_class;
     $("#right_tabs ul").append("<li class='" + liClass + "'>&nbsp;</li>");
 }
 
@@ -42,7 +51,7 @@ $("#right_tabs ul>li").each(function(i){
 
     // We define the value for moving the pianokey to the left
     pianokey_left = diff_max_height;
-    
+
     // Rotation process
     deg = 0;
     if (diff_max_height > 0) {
@@ -58,10 +67,10 @@ $("#right_tabs ul>li").each(function(i){
         }
         else if (deg > 90) {
             // deg and pianokey_top are static values
-            lag = 0;
-            if (pianokey_top % ((pianokey_height + pianokey_margin) * 2) == 0) {
-                lag = (pianokey_white_width - pianokey_black_width) / 4;
-            }
+            lag = (this.classList[0] == "white") ? 0 : pianokey_black_adjust;
+            //if (pianokey_top % ((pianokey_height + pianokey_margin) * 2) == 0) {
+                //lag = (pianokey_white_width - pianokey_black_width) / 4;
+            //}
             pianokey_top = max_height + border_radius + pianokey_height + pianokey_margin - lag;
             pianokey_left -= lag;
             deg = 90;
@@ -78,3 +87,45 @@ $("#right_tabs ul>li").each(function(i){
         rotate: deg
     }, 0);
 });
+
+
+function Setup() {
+    //if (!document.getElementById) return;
+    for (i=0; i<pianokey_sounds.length; i++) {
+        // embed the appropriate sound using document.write
+        document.write('<embed id="' + 'note_' + pianokey_sounds[i] + '"');
+        document.write(' src="/site_media/static/sounds/' + pianokey_sounds[i] + '.au" width="1" height="1"');
+        document.write(' style="position: fixed; top:0" autostart="false" enablejavascript="true">');
+    }
+    // Set up event handlers and embed the sounds
+    pianokeys_li = document.getElementsByClassName("white");
+    for (i=0; i<pianokeys_li.length; i++) {
+        // set up the event handler
+        pianokeys_li[i].onmouseover = PlaySound;
+    }
+    pianokeys_li = document.getElementsByClassName("black");
+    for (i=0; i<pianokeys_li.length; i++) {
+        // set up the event handler
+        pianokeys_li[i].onmouseover = PlaySound;
+    }
+}
+function PlaySound(e) {
+  if (!e) var e = window.event;
+  // which key was clicked?
+  thiskey = (e.target) ? e.target: e.srcElement;
+  // We absolutely need to put the white/black class in first position !!!
+  var sound = document.getElementById("note_" + thiskey.classList[1]);
+  try {
+    // RealPlayer
+    sound.DoPlay();
+  } catch (e) {
+    try {
+      // Windows Media / Quicktime
+      sound.Play();
+    } catch (e) {
+      //alert("No sound support.");
+    }
+  }
+}
+//Run the setup routine when this script executes
+Setup();
