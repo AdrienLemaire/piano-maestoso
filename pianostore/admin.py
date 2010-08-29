4b284870120d67db0187fbedd05e53e350d9f7e8
Adrien Lemaire <lemaire.adrien@gmail.com>
2010-08-29 14:26:36 +0200

    commit

--- pianostore/admin.py
+++ pianostore/admin.py
@@ -3,9 +3,9 @@ from models import Track
 
 
 class TrackAdmin(admin.ModelAdmin):
-    list_display = ('original_track',
-                    'title', 'title_slug', 'artist',
+    list_display = ('original_track', 'title', 'title_slug', 'artist',
                     'composer', 'category', 'description', 'date_added',
                     'adder', 'image')
+
 
 admin.site.register(Track, TrackAdmin)
