34569d0d24aced626fb85497d1cdf3428b052a09
Adrien Lemaire <lemaire.adrien@gmail.com>
2010-10-22 18:06:29 +0000

    uwsgi config bug

--- django_wsgi.py
+++ django_wsgi.py
@@ -1,13 +1,14 @@
 import os
 import django.core.handlers.wsgi
 import sys
-logfile = open("/home/fandekasp/piano-maestoso/tmp/path.log", "a")
+from django.conf import settings
+
+logfile = open(settings.PROJECT_ROOT + "/tmp/path.log", "a")
 for path in sys.path:
     logfile.write(path + "\n")
 logfile.close()
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'piano-maestoso.settings'
 
-from django.conf import settings
 sys.path.insert(0, os.path.join(settings.PROJECT_ROOT, "apps"))
 
 application = django.core.handlers.wsgi.WSGIHandler()
